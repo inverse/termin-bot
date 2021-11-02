e9bad855aec750f898efc81a4e1fa46d98361a3e
Malachi Soord <inverse.chi@gmail.com>
2021-11-02 21:53:34 +0100

    Init

---
+++ bot.py
@@ -0,0 +1,21 @@
+from environs import Env
+from telegram.ext import Updater
+import logging
+from telegram.ext import CommandHandler
+
+logging.basicConfig(level=logging.INFO)
+
+env = Env()
+env.read_env()
+
+updater = Updater(token=env("BOT_TOKEN"), use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
