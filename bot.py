7e17322595374bb730d92efdf20de3703829d701
Malachi Soord <inverse.chi@gmail.com>
2021-11-09 22:53:59 +0100

    Entity (#1)
    * Entitiy
    
    * Add logic
    
    * More tinker
    
    * More bot work

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 import logging
+from os.path import abspath
 
 from environs import Env
 from telegram.ext import CommandHandler, Updater
 
-from termin_bot.commands import Commands, command_help, command_start
+from termin_bot.commands import (Commands, command_list, command_start,
+                                 command_subscribe, command_subscriptions,
+                                 command_unsubscribe)
+from termin_bot.models import setup_database
 
 
 def main():
@@ -12,10 +16,17 @@ def main():
     env = Env()
     env.read_env()
 
+    setup_database(abspath(env("DB_PATH")))
+
     updater = Updater(token=env("BOT_TOKEN"), use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler(Commands.START, command_start))
-    dispatcher.add_handler(CommandHandler(Commands.HELP, command_help))
+    dispatcher.add_handler(CommandHandler(Commands.LIST, command_list))
+    dispatcher.add_handler(CommandHandler(Commands.SUBSCRIBE, command_subscribe))
+    dispatcher.add_handler(CommandHandler(Commands.UNSUBSCRIBE, command_unsubscribe))
+    dispatcher.add_handler(
+        CommandHandler(Commands.SUBSCRIPTIONS, command_subscriptions)
+    )
     updater.start_polling()
     updater.idle()
 
--- termin_bot/commands.py
+++ termin_bot/commands.py
@@ -1,25 +1,104 @@
-from telegram import ForceReply, Update
+from telegram import Update
 from telegram.ext import CallbackContext
+
+
+class Appointments:
+    APPOINTMENTS = [
+        "Anmeldung einer Wohnung 1",
+        "Anmeldung einer Wohnung 2",
+    ]
+
+    def get_commands(self) -> list:
+        return [
+            appointment.replace(" ", "_").lower() for appointment in self.APPOINTMENTS
+        ]
+
+    def get_commands_dict(self) -> dict:
+        return {
+            appointment.replace(" ", "_").lower(): appointment
+            for appointment in self.APPOINTMENTS
+        }
+
+
+APPOINTMENTS = Appointments()
 
 
 class Commands:
     START = "start"
-    HELP = "help"
+    LIST = "list"
+    SUBSCRIBE = "subscribe"
+    UNSUBSCRIBE = "unsubscribe"
+    SUBSCRIPTIONS = "subscriptions"
 
 
 def command_start(update: Update, _context: CallbackContext):
     user = update.effective_user
 
     welcome_text = fr"""
-Hi {user.name}\!
+Hi {user.name},
 
-Welcome to the Berlin Termin Bot\.
+Welcome to the Berlin Termin Bot\!
 
 A place where you can get notified for free appointments on the Berlin Services website\.
+
+Start by issuing `/list` to see available appointment types\.
+
+Then subscribe with `/subscribe <appointment>`\.
+
+To unregister use `/unregister <appointment`\.
+
+To list your subscriptions use `/subscribed`
     """
 
     update.message.reply_markdown_v2(welcome_text)
 
 
-def command_help(update: Update, _context: CallbackContext):
-    update.message.reply_text("Help coming soon....")
+def command_list(update: Update, _context: CallbackContext):
+
+    type_text = ""
+    for command, label in APPOINTMENTS.get_commands_dict().items():
+        type_text += f"\- `{command}` \({label}\)\n"
+
+    list_text = f"""
+Here are the available types:
+{type_text}
+    """
+
+    update.message.reply_markdown_v2(list_text)
+
+
+def command_subscribe(update: Update, context: CallbackContext):
+    if len(context.args) == 0:
+        update.message.reply_text("You must provide appointment")
+
+        return
+
+    appointment = context.args[0]
+
+    if appointment not in APPOINTMENTS.get_commands():
+        update.message.reply_text(
+            f"{appointment} not in list of available appointments"
+        )
+
+        return
+
+    # TODO: Model logic
+
+    update.message.reply_text(f"Successfully subscribed to {appointment}")
+
+
+def command_unsubscribe(update: Update, context: CallbackContext):
+    if len(context.args) == 0:
+        update.message.reply_text("You must provide appointment")
+
+        return
+
+    appointment = context.args[0]
+
+    # TODO: Model logic
+
+    update.message.reply_text(f"Successfully subscribed to {appointment}")
+
+
+def command_subscriptions(update: Update, _context: CallbackContext):
+    update.message.reply_text("Coming soon...")
