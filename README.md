327594049d2a509d55a2626c9c7c5d941d0c7f30
Malachi Soord <inverse.chi@gmail.com>
2021-12-07 23:07:58 +0100

    Consolidate entry point (#9)
    * Consolidate entry point
    
    * Fix db access

--- README.md
+++ README.md
@@ -9,3 +9,29 @@ You can interact with the production bot at `@TBD`
 Create a Telegram bot following the [official documentation](https://core.telegram.org/bots#3-how-do-i-create-a-bot).
 
 Copy `.env.example` to `.env` and provide your newly created bot token in `BOT_TOKEN`, as well as configuring a suitable path for the persistance layer.
+
+### Bot
+
+You can start the bot using:
+
+```bash
+python cli.py bot
+```
+
+### Scraping
+
+Scrape appointments using:
+
+```bash
+python cli.py scrape-appointments
+```
+
+### Processing
+
+Process subscriptions using:
+
+```bash
+python cli.py process-subscriptions
+```
+
+You will probably want to run this on some cron schedule as this is what will be scraping the appointments and sending notifications.
--- cli.py
+++ cli.py
@@ -1,16 +1,49 @@
 import click
 from pony.orm import db_session
+from telegram.ext import CommandHandler, Updater
 
 from termin_bot import appointment_handler, common, model, scraper
 
 
-@db_session
 @click.group()
 def cli():
     pass
 
 
 @cli.command()
+@db_session
+def bot():
+    """Start the bot"""
+    env = common.get_env()
+    updater = Updater(token=env("BOT_TOKEN"), use_context=True)
+    dispatcher = updater.dispatcher
+
+    from termin_bot.commands import (
+        Commands,
+        command_list,
+        command_start,
+        command_subscribe,
+        command_subscriptions,
+        command_uninstall,
+        command_unsubscribe,
+    )
+
+    dispatcher.add_handler(CommandHandler(Commands.START, command_start))
+    dispatcher.add_handler(CommandHandler(Commands.LIST, command_list))
+    dispatcher.add_handler(CommandHandler(Commands.SUBSCRIBE, command_subscribe))
+    dispatcher.add_handler(
+        CommandHandler(Commands.UNSUBSCRIBE, command_unsubscribe)
+    )
+    dispatcher.add_handler(
+        CommandHandler(Commands.SUBSCRIPTIONS, command_subscriptions)
+    )
+    dispatcher.add_handler(CommandHandler(Commands.UNINSTALL, command_uninstall))
+    updater.start_polling()
+    updater.idle()
+
+
+@cli.command()
+@db_session
 def scrape_appointments():
     """Scrape available appointments."""
     click.echo("Started scraping appointments")
@@ -20,6 +53,7 @@ def scrape_appointments():
 
 
 @cli.command()
+@db_session
 def process_subscriptions():
     """Process subscriptions."""
     click.echo("Started processing subscriptions")
